8b9fb46f4a974ec8b2a95c63ac294b1ffa04981c
coinlist123415-lab <coinlist123415@gmail.com>
2025-11-13 12:07:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+// This file is for additional JavaScript functionality
+// Most functionality is already included in the HTML files
+
+console.log('Aviation Security LMS loaded successfully!');
+
+// Additional functions can be added here as needed
+function simulateTest() {
+    alert('This would start a real test in the full system!');
+}
+
+function viewCertificate() {
+    alert('Certificate would be displayed or downloaded here!');
+}
+
+function enrollInCourse() {
+    alert('You have been enrolled in the course!');
+}
